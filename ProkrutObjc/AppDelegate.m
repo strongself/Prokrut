61162f8ab365b8e66f397bb92aedbadb48764515
Tsyganov Stanislav <alloy.stas@gmail.com>
2016-07-25 11:41:01 +0300

    Update AppDelegate.m

--- ProkrutObjc/AppDelegate.m
+++ ProkrutObjc/AppDelegate.m
@@ -22,8 +22,6 @@
     // Override point for customization after application launch.
     
     ProkrutobjcKeys *prokrutKeys = [ProkrutobjcKeys new];
-    NSString *applicationId = @"";
-    NSString *clientKey = @"";
     
     [Parse enableLocalDatastore];
     [Parse setApplicationId:applicationId
