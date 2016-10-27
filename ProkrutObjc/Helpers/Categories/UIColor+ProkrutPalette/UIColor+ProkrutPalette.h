78895da59ad890002f07900040e632d09baa326d
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-10-27 21:07:03 +0300

    Добавил категорию с цветами

---
+++ ProkrutObjc/Helpers/Categories/UIColor+ProkrutPalette/UIColor+ProkrutPalette.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+ProkrutPalette.h
+//  ProkrutObjc
+//
+//  Created by Egor Tolstoy on 27/10/16.
+//  Copyright © 2016 DevAlloy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ProkrutPalette)
+
++ (UIColor *)prokrutGreenColor;
++ (UIColor *)prokrutRedColor;
+
+@end
