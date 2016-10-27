743222e7daee629c198074521863f36c6cb5b98c
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-10-27 11:27:33 +0300

    Добавлен дизайн ячейки статистики

---
+++ ProkrutObjc/Classes/PresentationLayer/UserStories/StatisticsUserStory/Modules/StatisticsModule/View/Cells/PlayerStatisticsTableViewCell.h
@@ -0,0 +1,22 @@
+//
+//  PlayerStatisticsTableViewCell.h
+//  ProkrutObjc
+//
+//  Created by Egor Tolstoy on 26/10/16.
+//  Copyright © 2016 DevAlloy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerStatisticsTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *emailLabel;
+@property (weak, nonatomic) IBOutlet UILabel *winratePercent;
+@property (weak, nonatomic) IBOutlet UIProgressView *winrateProgress;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *winsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *lossesLabel;
+
+@end
---
+++ ProkrutObjc/Classes/PresentationLayer/UserStories/StatisticsUserStory/Modules/StatisticsModule/View/Cells/PlayerStatisticsTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  PlayerStatisticsTableViewCell.m
+//  ProkrutObjc
+//
+//  Created by Egor Tolstoy on 26/10/16.
+//  Copyright © 2016 DevAlloy. All rights reserved.
+//
+
+#import "PlayerStatisticsTableViewCell.h"
+
+@implementation PlayerStatisticsTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
