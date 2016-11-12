f8b6e2ffcbb12a2d7231165fc33a7825cd421dbc
Karpushin Artem <a.karpushin@rambler-co.ru>
2016-11-12 14:55:29 +0300

    Добавил PlayerProfileModuleConfiguration

---
+++ ProkrutObjc/PlayerProfileUserStory/PlayerProfile/Presenter/ModuleConfiguration/PlayerProfileModuleConfiguration.h
@@ -0,0 +1,18 @@
+//
+//  PlayerProfileModuleConfiguration.h
+//  ProkrutObjc
+//
+//  Created by Karpushin Artem on 12/11/16.
+//  Copyright © 2016 DevAlloy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ @author Artem Karpushin
+ 
+ Объект содержит данные необходимые для конфигурации модуля "Профиль игркока"
+ */
+@interface PlayerProfileModuleConfiguration : NSObject
+
+@end
---
+++ ProkrutObjc/PlayerProfileUserStory/PlayerProfile/Presenter/ModuleConfiguration/PlayerProfileModuleConfiguration.m
@@ -0,0 +1,13 @@
+//
+//  PlayerProfileModuleConfiguration.m
+//  ProkrutObjc
+//
+//  Created by Karpushin Artem on 12/11/16.
+//  Copyright © 2016 DevAlloy. All rights reserved.
+//
+
+#import "PlayerProfileModuleConfiguration.h"
+
+@implementation PlayerProfileModuleConfiguration
+
+@end
